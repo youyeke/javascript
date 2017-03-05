6c831f6806f99fa5191b8d6ce328211bf3b3c7a3
youyeke <youyeke@163.com>
2017-03-06 00:44:29 +0800

    基本框架重构完成
    基本框架重构完成，手动切换CSS动画切换实现，自动轮播未写，JS动画未写。

---
+++ imageCarousel/js/imageCarousel.js
@@ -0,0 +1,113 @@
+(function(window,undefined) {
+	window.ImageCarousel = function(obj){
+		this.dom = obj.dom;
+		this.width = obj.width || 800;
+		this.heigth = obj.heigth || 450;
+		this.imageList = obj.imageList;
+		this.titleList = obj.titleList;
+		this.path = obj.path;
+		this.speed = obj.speed;
+		this.sleep = obj.sleep;
+		this.hiddenTitle = (obj.hiddenTitle == true)? true : false;
+		this.animationType = obj.animationType || "JS";
+	};
+	ImageCarousel.prototype.ready = function(){
+		//把图片载入缓存，并绘制dom
+		var dom = this.dom,
+			width = this.width,
+			heigth = this.heigth,
+			imageList = this.imageList,
+			titleList = this.titleList;
+			hiddenTitle = this.hiddenTitle
+		var imageCacheCompleteAmount = 0;
+		var that = this;
+		imageList.forEach(function(url,index){
+			var img = new Image();
+			img.src = url;
+			if(img.complete){
+				imageCacheCompleteAmount++;
+				console.log("cache:",img)
+				return ;
+			}
+			img.onload = function(){
+				imageCacheCompleteAmount++;
+				if(imageCacheCompleteAmount === imageList.length){
+					createDom();
+				}
+				return ;
+			}
+		});
+		function createDom(){
+			dom.classList = "imageCarousel";
+			var domImage = "",
+				domImageSwith = "";
+			imageList.forEach(function(url,index){
+				domImage += "<li><img src="+url+"></li>";
+				if(index ==0){
+					domImageSwith += "<span class=\"activeImg\">"+(index+1)+"</span>";
+				}else{
+					domImageSwith += "<span>"+(index+1)+"</span>";
+				}
+			});
+			dom.innerHTML = [
+				"<ul>",
+					domImage,
+				"</ul>",
+				"<div class=\"IC-switchImg\">",
+					domImageSwith,
+				"</div>",
+				"<div class=\"IC-title\">",
+	    		"<p>"+titleList[0]+"</p>",
+	    		"</div>"
+			].join("");
+			if(!hiddenTitle){
+				dom.childNodes[2].style.bottom ="0";
+			}
+			console.log("%O",dom)
+			var switchImage = dom.childNodes[1];
+			that.animation();//初始化方法，以便惰性选择CSS动画或是JS动画
+			switchImage.addEventListener("click",function(e){
+				if(e.target.tagName == "SPAN"){
+					that.animation(e.target.innerHTML);
+					that.switchStyle(e.target.innerHTML-1);
+				}
+			})
+		}
+	};
+	ImageCarousel.prototype.animation = function(){
+		var animationType = this.animationType,
+			dom = this.dom,
+			width = this.width,
+			length = this.imageList.length,
+			speed = this.speed;
+		var currentIndex = 0;
+		if(animationType =="CSS"){
+			dom.childNodes[0].style.transition = "all "+speed+"ms";
+			dom.childNodes[0].style.left = "0px"
+			this.animation = function(index){
+				if(index == undefined){
+					if(currentIndex < length-1){
+						currentIndex++;
+					}else{
+						currentIndex = 0;
+					}
+				}else{
+					currentIndex = index-1;
+				}
+				dom.childNodes[0].style.left = -currentIndex*width+"px";
+			}
+		}else{
+			this.animation = function(index){
+				console.log("JS",index)
+			}
+		}
+	};
+	ImageCarousel.prototype.switchStyle = function(index){
+		var dom = this.dom,
+			switchButton = dom.childNodes[1];
+		[].forEach.call(switchButton.children,function(child,index){
+			child.classList = "";
+		});
+		switchButton.childNodes[index].classList = "activeImg";
+	}
+})(window)
